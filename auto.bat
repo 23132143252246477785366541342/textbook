49308009828c6c55e25afda20ed138c41d48bc2a
R <Tommyalbert899@gmail.com.com>
2021-07-06 10:57:22 -0400

    auto push

---
+++ auto.bat
@@ -0,0 +1,31 @@
+:loop
+	
+	:: Navigate to the directory you wish to push to GitHub
+	::Change <path> as needed. Eg. C:\Users\rich\Desktop\Writings
+	cd "C:\Users\Nam Tran\Desktop\Coding\C++\"
+	
+	::Initialize GitHub
+	git init
+	
+	::Pull any external changes (maybe you deleted a file from your repo?)
+	git pull
+	
+	::Add all files in the directory
+	git add --all
+	
+	::Commit all changes with the message "auto push". 
+	::Change as needed.
+	git commit -m "auto push"
+	
+	::Push all changes to GitHub 
+	git push "https://github.com/23132143252246477785366541342/textbook.git"
+	
+	::Alert user to script completion and relaunch.
+	echo Complete. Relaunching...
+	
+	::Wait 300 seconds until going to the start of the loop.
+	::Change as needed.
+	TIMEOUT 300
+	
+::Restart from the top.	
+goto loop
