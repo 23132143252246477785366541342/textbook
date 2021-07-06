4e766e58dffeed56066f04e5a31fffadbd27f1e8
Tony Tran <tommyalbert899@gmail.com>
2021-07-06 17:49:22 -0400

    auto push

--- C++_Textbook/Chapter_2/casting.cpp
+++ C++_Textbook/Chapter_2/casting.cpp
@@ -12,11 +12,12 @@ using namespace std;
 
 int main() {
     float f = 1.232323;
-    cout << "static_cast<int>(7.9) = " << endl;
-    cout << "static_cast<int>(3.3) = " << endl;
-    cout << "static_cast<double>(25) = " << endl;
-    cout << "static_cast<double>(5 + 3) = " << endl;
-    cout << "static_cast<double>(15) / 2 = " << endl;
-    cout << "static_cast<double>(15 / 2) = " << endl;
-    cout << "static_cast<int>(7.8 + static_cast<double>(15) / 2) = " << endl;
+    cout << "static_cast<int>(7.9) = " << static_cast<int>(7.9) << endl;
+    cout << "static_cast<int>(3.3) = " << static_cast<int>(3.3) << endl;
+    cout << "static_cast<double>(25) = " << static_cast<double>(25) << endl;
+    cout << "static_cast<double>(5 + 3) = " << static_cast<double>(5 + 3) << endl;
+    cout << "static_cast<double>(15) / 2 = " << static_cast<double>(15) / 2 << endl;
+    cout << "static_cast<double>(15 / 2) = " << static_cast<double>(15 / 2) << endl;
+    cout << "static_cast<int>(7.8 + static_cast<double>(15) / 2) = " << static_cast<int>(7.8 + static_cast<double>(15) / 2) << endl;
+
 }
