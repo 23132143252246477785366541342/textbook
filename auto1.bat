56d58fa95010b869f679226b0e3bccbf9cd10c87
R <tommyalbert899@gmail.com>
2021-07-07 10:02:50 -0400

    auto push

--- auto1.bat
+++ auto1.bat
@@ -2,17 +2,17 @@
 	
 	:: Navigate to the directory you wish to push to GitHub
 	::Change <path> as needed. Eg. C:\Users\rich\Desktop\Writings
-	cd "C:\Users\Nam Tran\Desktop\Coding\C++\C++_Textbook"
+	cd "C:\Users\Nam Tran\Desktop\Coding\C++\textbook\"
 	
 	::Initialize GitHub
 	git init
 	
 	::Pull any external changes (maybe you deleted a file from your repo?)
-
+    git pull
 	
 	::Add all files in the directory
-
-        git config --global user.email "tommyalbert899@gmail.com"
+    git branch -m Main
+    git config --global user.email "tommyalbert899@gmail.com"
 	git add .
 	
 	::Commit all changes with the message "auto push". 
@@ -20,8 +20,8 @@
 	git commit -m "auto push"
 	
 	::Push all changes to GitHub 
-	git push "https://github.com/23132143252246477785366541342/textbook.git"
-	
+	git remote add origin "https://github.com/23132143252246477785366541342/textbook.git"
+	git push -u origin Main
 	::Alert user to script completion and relaunch.
 	echo Complete. Relaunching...
 	
